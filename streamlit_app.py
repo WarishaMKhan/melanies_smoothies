4b5eda2cad0770c33c1078718cfb4b387463dd0e
WarishaMKhan <164744144+WarishaMKhan@users.noreply.github.com>
2024-08-22 01:38:13 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ if time_to_insert and ingredients_list:
     # Prepare the ingredients string
     ingredients_string = ' '.join(ingredients_list)
     for fruit_chosen in ingredients_list:
-            ingredients_string +=fruit_chosen + ' "
+            ingredients_string +=fruit_chosen + ' '
             search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
             st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
