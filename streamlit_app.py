937a89a5c276dc2b58f0f3536939e69a79e61523
WarishaMKhan <164744144+WarishaMKhan@users.noreply.github.com>
2024-08-22 01:43:43 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,8 @@ pd_df=my_dataframe.to_pandas()
 #st.dataframee=(pd_df)
 #st.stop()
 # Multi-select input for ingredients
-ingredients_list = st.multiselect('Choose up to 5 ingredients: ', my_dataframe)
+ingredients_list = st.multiselect
+('Choose up to 5 ingredients: ', my_dataframe,max_selection=5)
 
 # Button to submit the order
 time_to_insert = st.button('Submit Order')
